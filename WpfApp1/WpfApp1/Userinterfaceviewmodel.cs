c9efbd4de2e8416cae2a3431e2af2fb8de004bf6
JamesSchultz95 <70610826+JamesSchultz95@users.noreply.github.com>
2020-09-29 08:48:20 -0500

    add edit window

--- WpfApp1/WpfApp1/Userinterfaceviewmodel.cs
+++ WpfApp1/WpfApp1/Userinterfaceviewmodel.cs
@@ -14,7 +14,7 @@ namespace WpfApp1
     public class Userinterfaceviewmodel
     {
         private List<Passdata> _passitems = new List<Passdata>();
-        private editwindow ew;
+        private editwindow ew = new editwindow();
         public List<Passdata> PassItems
         {
             get
@@ -38,23 +38,19 @@ namespace WpfApp1
             _passitems.Add(new Passdata("pass #1",new DateTime(2016, 7, 15, 0, 0, 0), new DateTime(2016, 7, 15, 2, 7, 59)));
             _passitems.Add(new Passdata("pass #2", new DateTime(2016, 7, 15, 2, 7, 59), new DateTime(2016, 7, 15, 4, 15, 58)));
             Passdata temp = _passitems.ElementAt<Passdata>(1);
-            temp.sunlight.startTime = new DateTime(2016, 7, 15, 2, 7, 59);
-            temp.sunlight.endTime = new DateTime(2016, 7, 15, 3, 7, 59);
-            temp.mission.startTime = new DateTime(2016, 7, 15, 3, 7, 59);
-            temp.mission.endTime = new DateTime(2016, 7, 15, 3, 37, 59);
-            temp.encryption.startTime = new DateTime(2016, 7, 15, 3, 37, 59);
-            temp.encryption.endTime = new DateTime(2016, 7, 15, 4, 7, 59);
-            temp.Datalink.startTime = new DateTime(2016, 7, 15, 4, 7, 59);
-            temp.Datalink.endTime = new DateTime(2016, 7, 15, 5, 7, 59);
+            temp.sunlight.Duration = new TimeSpan(1,0,0);
+            temp.mission.Duration = new TimeSpan(0, 30, 0);
+            temp.encryption.Duration = new TimeSpan(0, 30, 0);
+            temp.Datalink.Duration = new TimeSpan(0, 8, 0);
             SelectedPassItem = _passitems.ElementAt<Passdata>(0);
             EditClick = new RelayCommand(new Action<object>(HandleEditClicked));
         }
 
         private void HandleEditClicked(object something)
         {
-            if (ew == null)
+            if (!ew.IsActive)
             {
-                editwindow ew = new editwindow();
+                ew = new editwindow();
                 ew.DataContext = this;
                 ew.Show();
             }
