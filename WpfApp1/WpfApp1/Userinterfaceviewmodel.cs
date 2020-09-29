cbba95bd400acc459051468979924bb23f4cbf1a
JamesSchultz95 <70610826+JamesSchultz95@users.noreply.github.com>
2020-09-29 09:19:39 -0500

    add observable collection

--- WpfApp1/WpfApp1/Userinterfaceviewmodel.cs
+++ WpfApp1/WpfApp1/Userinterfaceviewmodel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -11,11 +12,14 @@ using System.Windows.Media;
 
 namespace WpfApp1
 {
-    public class Userinterfaceviewmodel
+    public class Userinterfaceviewmodel: INotifyPropertyChanged
     {
-        private List<Passdata> _passitems = new List<Passdata>();
+        private ObservableCollection<Passdata> _passitems = new ObservableCollection<Passdata>();
         private editwindow ew = new editwindow();
-        public List<Passdata> PassItems
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Passdata> PassItems
         {
             get
             {
@@ -28,9 +32,23 @@ namespace WpfApp1
             set;
         }
 
+        public ICommand AddClick
+        {
+            get;
+            set;
+        }
+        private Passdata _SelectedPassItem;
         public Passdata SelectedPassItem
         {
-            get;set;
+            get
+            {
+                return _SelectedPassItem;
+            }
+            set
+            {
+                _SelectedPassItem = value;
+                OnPropertyChanged("SelectedPassItem");
+            }
         }
 
         public Userinterfaceviewmodel()
@@ -43,7 +61,9 @@ namespace WpfApp1
             temp.encryption.Duration = new TimeSpan(0, 30, 0);
             temp.Datalink.Duration = new TimeSpan(0, 8, 0);
             SelectedPassItem = _passitems.ElementAt<Passdata>(0);
+            PassItems.CollectionChanged += OnPassItemsCollectionChanged;
             EditClick = new RelayCommand(new Action<object>(HandleEditClicked));
+            AddClick = new RelayCommand(new Action<object>(HandleAddClicked));
         }
 
         private void HandleEditClicked(object something)
@@ -54,6 +74,26 @@ namespace WpfApp1
                 ew.DataContext = this;
                 ew.Show();
             }
+        }
+        private void HandleAddClicked(object something)
+        {
+            _passitems.Add(new Passdata("pass #"+ (_passitems.Count+1), new DateTime(2016, 7, 15, 2, 7, 59), new DateTime(2016, 7, 15, 4, 15, 58)));
+            Passdata temp = _passitems[_passitems.Count -1];
+            temp.sunlight.Duration = new TimeSpan(1, 0, 0);
+            temp.mission.Duration = new TimeSpan(0, 30, 0);
+            temp.encryption.Duration = new TimeSpan(0, 30, 0);
+            temp.Datalink.Duration = new TimeSpan(0, 8, 0);
+        }
+            private void OnPassItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged();
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
