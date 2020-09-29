809ceafe4fe732bf49c29824215f61f2f6fc7a14
John Coan <jvc0003@uah.edu>
2020-09-29 11:35:38 -0600

    adding powermeter to master

---
+++ WpfApp1/WpfApp1/Power.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Power.xaml
+    /// </summary>
+    public partial class Power : UserControl
+    {
+        public Power()
+        {
+            InitializeComponent();
+        }
+    }
+}
