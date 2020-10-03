b5bd0284682d3f7f27c69e81a18d052b101a1713
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-10-03 02:41:23 -0500

    Initial implementation of ribbon

--- SchedulerGUI/Views/MainWindow.xaml.cs
+++ SchedulerGUI/Views/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ namespace SchedulerGUI.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Fluent.RibbonWindow
     {
         public MainWindow()
         {
