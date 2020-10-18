7c4ca7cb9b9700e093eb2af19616ce9e4fb24e69
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-10-18 16:25:46 -0500

    Added names for accelerator type in dropdown in plot tool

---
+++ SchedulerGUI/Converters/AcceleratorToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SchedulerDatabase.Extensions;
+using SchedulerDatabase.Models;
+
+namespace SchedulerGUI.Converters
+{
+    /// <summary>
+    /// <see cref="AcceleratorToStringConverter"/> provides a converter to display <see cref="AESEncyptorProfile.AcceleratorType"/>s
+    /// in a user-friendly string format.
+    /// </summary>
+    public sealed class AcceleratorToStringConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string enumStr && Enum.TryParse<AESEncyptorProfile.AcceleratorType>(enumStr, out var acc))
+            {
+                return acc.ToFriendlyName();
+            }
+
+            return string.Empty;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
