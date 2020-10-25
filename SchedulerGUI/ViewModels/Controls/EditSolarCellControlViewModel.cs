ee49595424591df88e2c6056102e075f79b6851c
John Coan <jvc0003@uah.edu>
2020-10-25 12:09:29 -0600

    Update the solar vm to have a starting point when the passes are added in

--- SchedulerGUI/ViewModels/Controls/EditSolarCellControlViewModel.cs
+++ SchedulerGUI/ViewModels/Controls/EditSolarCellControlViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using GalaSoft.MvvmLight;
+using SchedulerGUI.Models;
 
 namespace SchedulerGUI.ViewModels.Controls
 {
@@ -9,10 +11,32 @@ namespace SchedulerGUI.ViewModels.Controls
     /// </summary>
     public class EditSolarCellControlViewModel : ViewModelBase
     {
+        private IEnumerable<PassOrbit> passes;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="EditSolarCellControlViewModel"/> class.
         /// </summary>
         public EditSolarCellControlViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the pass data that should be used to build the historical display.
+        /// </summary>
+        public IEnumerable<PassOrbit> Passes
+        {
+            get => this.passes;
+            set
+            {
+                this.passes = value;
+                this.InitDefaultSolarData();
+            }
+        }
+
+        /// <summary>
+        /// Modifies the initial passes based on the defaults of the edit solar control vm.
+        /// </summary>
+        private void InitDefaultSolarData()
         {
         }
     }
