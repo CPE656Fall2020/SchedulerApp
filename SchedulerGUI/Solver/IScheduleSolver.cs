9b3d147d6d97caeb3ab4f984a908cd1cff1f242c
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-10-31 12:40:00 -0500

    First implementation of scheduler solving for encryption and datalink

--- SchedulerGUI/Solver/IScheduleSolver.cs
+++ SchedulerGUI/Solver/IScheduleSolver.cs
@@ -23,9 +23,10 @@ namespace SchedulerGUI.Solver
         /// Computes a solution to the scheduling problem, and stores the final energy values in <see cref="PassOrbit"/>.
         /// </summary>
         /// <param name="passes">The orbital passes and associated constraints that need scheduled.</param>
-        /// <param name="availableProfiles">The AES profiles describing the devices that are available to schedule for.</param>
+        /// <param name="summarizedEncryptors">The byte processing encryption profiles describing the summarized devices that are available to schedule for.</param>
+        /// <param name="summarizedCompressors">The byte processing compression profiles describing the summarized devices that are available to schedule for.</param>
         /// <param name="battery">The specification for the battery powering the satellite.</param>
         /// <returns>A <see cref="ScheduleSolution"/> describing if the scenerio is possible and which device are required.</returns>
-        ScheduleSolution Solve(IEnumerable<PassOrbit> passes, IEnumerable<AESEncryptorProfile> availableProfiles, Battery battery);
+        ScheduleSolution Solve(IEnumerable<PassOrbit> passes, IEnumerable<IByteStreamProcessor> summarizedEncryptors, IEnumerable<IByteStreamProcessor> summarizedCompressors, Battery battery);
     }
 }
