184ecd7194c59e0a6ce08519c7c345b7d75d2f8c
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-11-03 00:19:55 -0600

    Std. Dev. for joules on the profile graph shows a metric formatted unit

---
+++ SchedulerGUI/Converters/MetricToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SchedulerDatabase.Helpers;
+
+namespace SchedulerGUI.Converters
+{
+    /// <summary>
+    /// <see cref="MetricToStringConverter"/> provides a converter to display metric values as a user-friendly string.
+    /// </summary>
+    public sealed class MetricToStringConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double v)
+            {
+                return MetricUtils.MetricValueAxisLabelFormatter(v, parameter?.ToString(), false);
+            }
+
+            return string.Empty;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
